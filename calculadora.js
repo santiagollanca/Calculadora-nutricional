1eb67262ca6f792939711c9ad9a807470d6d046e
santiagollanca <santiagollanca@gmail.com>
2023-07-06 12:21:07 -0300

    ultima actualizacion

--- calculadora.js
+++ calculadora.js
@@ -1,18 +1,41 @@
 var bebidaselec="";guarnicionselec="";principalselec="";bebidacant="";guarnicioncant="";principalcant="";
 
-let contador = (valor) => {
-    
+let contadorprincipal = (valor) => {
+    let proteinas =0
+    let calorias =0
     let gramos = parseFloat(valor);
-    let proteinas = gramos * 0.2; 
+    let gramos2 = parseFloat(valor);
+    
+    if(principalselec=="pollo") {
+         proteinas= gramos * 0.2; 
+        calorias = gramos2 * 1.07;
+    }
+    if(principalselec=="pizza") {
+        proteinas = gramos * 0.5; 
+        calorias = gramos2 * 17;
+    }
+    if(principalselec=="pescado") {
+        proteinas = gramos * 3.5; 
+        calorias = gramos2 * 127;
+    }
+    if(principalselec=="carne") {
+        proteinas = gramos * 95; 
+        calorias = gramos2 * 170;
+    }
+    if(principalselec=="milanesa") {
+        proteinas = gramos * 12; 
+        calorias = gramos2 * 27;
+    }
+
     let resultadoProteinas = document.getElementById("resultado_proteinas");
     resultadoProteinas.innerHTML = "<p>" + proteinas + " g</p>";
-
-    let gramos2 = parseFloat(valor);
-    let calorias = gramos2 * 1.07;
     let resultadoCalorias = document.getElementById("resultado_calorias");
     resultadoCalorias.innerHTML = "<p>" + calorias + " g</p>";
+    
+    console.log(principalselec+""+principalcant)  
+};
 
-  };
+
 
 
   
